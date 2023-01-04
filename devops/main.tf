5939d7cd979421b146cd731b68eb4cd2516bfe8c
Justin Mitchel <j@teamcfe.com>
2023-01-03 20:54:38 -0600

    minimized terraform file

--- devops/main.tf
+++ devops/main.tf
@@ -19,60 +19,21 @@ locals {
 }
 
 variable "linode_api_token" {
-description = "Your Linode API Personal Access Token. (required)"
-  sensitive   = true
+    description = "Your Linode API Personal Access Token. (required)"
+    sensitive   = true
 }
 
-variable "k8s_version" {
-    description = "The Kubernetes version to use for this cluster. (required)"
-    default = "1.24"
-}
+resource "linode_lke_cluster" "terraform_k8s" {
+    k8s_version="1.24"
+    k8s_label="terraform-k8s"
+    k8s_region="us-east"
+    tags=["terraform-k8s"]
+    pool {
+        type  = "g6-standard-1"
+        count = 3
 
-variable "k8s_label" {
-    description = "The unique label to assign to this cluster. (required)"
-    default = "tf-k8s-cluster"
-}
-
-variable "k8s_region" {
-    description = "The region where your cluster will be located. (required)"
-    default = "us-east"
-}
-
-variable "tags" {
-    description = "Tags to apply to your cluster for organizational purposes."
-    type = list(string)
-    default = ["tf-k8s-cluster"]
-}
-
-variable "pools" {
-    description = "The Node Pool specifications for the Kubernetes cluster. (required)"
-    type = list(object({
-        type = string
-        count = number
-    }))
-    default = [
-        {
-            type = "g6-standard-1"
-            count = 3
-        }
-    ]
-}
-
-resource "linode_lke_cluster" "terraformk8s" {
-    k8s_version = var.k8s_version
-    label = var.k8s_label
-    region = var.k8s_region
-    tags = var.tags
-
-    dynamic "pool" {
-        for_each = var.pools
-        content {
-            type  = pool.value["type"]
-            count = pool.value["count"]
-        }
     }
 }
-
 
 resource "local_file" "k8s_config" {
     content = "${nonsensitive(base64decode(linode_lke_cluster.terraformk8s.kubeconfig))}"
